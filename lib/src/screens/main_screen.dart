c7f6aebbd5f7d7d699144a9aa6e766e1d0aa5b13
Gabriel Pagotto <gabriel.pagotto@icloud.com>
2023-10-27 19:22:30 -0300

    Implementando bottom sheet

--- lib/src/screens/main_screen.dart
+++ lib/src/screens/main_screen.dart
@@ -54,138 +54,218 @@ class _MainScreenState extends State<MainScreen> {
     final mediaQuery = MediaQuery.of(context);
 
     return Material(
-      child: Container(
-        color: Colors.white,
-        padding: EdgeInsets.only(top: mediaQuery.padding.top),
-        child: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [const Icon(Icons.menu, size: 30), IconButton(onPressed: () => print('clicas'), icon: const Icon(Icons.search))],
-              ),
+      child: Stack(
+        children: [
+          Container(
+            color: Colors.white,
+            padding: EdgeInsets.only(top: mediaQuery.padding.top),
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [const Icon(Icons.menu, size: 30), IconButton(onPressed: () => print('clicas'), icon: const Icon(Icons.search))],
+                  ),
+                ),
+                Expanded(
+                  child: ListView(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 20),
+                        child: Align(
+                          alignment: Alignment.topLeft,
+                          child: Text(
+                            _categoriaAtualNome,
+                            style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 26),
+                          ),
+                        ),
+                      ),
+                      Container(
+                        margin: const EdgeInsets.only(top: 20),
+                        child: Carrossel(
+                          key: _carroseelKey,
+                          currentIndex: _whiskysCurrentIndex,
+                          onChanged: _changeWhiskysCurrentIndex,
+                          childrens: categoriaMock.map((e) => CarrosselItem(e.nome)).toList(),
+                        ),
+                      ),
+                      Container(
+                        margin: const EdgeInsets.only(top: 30),
+                        width: double.infinity,
+                        height: _BEBIDA_CONTAINER_SIZE + _BEBIDA_DESCRICAO_HEIGHT + _BEBIDA_DESCRICAO_MARGIN_BOTTOM,
+                        child: ListView.builder(
+                          scrollDirection: Axis.horizontal,
+                          itemCount: _categoriaAtualBebidas.length,
+                          itemBuilder: (context, index) {
+                            final bebida = _categoriaAtualBebidas[index];
+                            return InkWell(
+                              onTap: () => _irParaDetalhes(bebida),
+                              child: Container(
+                                width: _fractionSize,
+                                margin: const EdgeInsets.symmetric(horizontal: 10),
+                                child: Column(
+                                  mainAxisSize: MainAxisSize.min,
+                                  children: [
+                                    SizedBox(
+                                      height: _BEBIDA_CONTAINER_SIZE,
+                                      child: Stack(
+                                        children: [
+                                          Positioned(
+                                            bottom: 0,
+                                            child: Container(
+                                              height: _fractionSize,
+                                              width: _fractionSize,
+                                              decoration: BoxDecoration(
+                                                color: bebida.cor.withOpacity(0.4),
+                                                borderRadius: const BorderRadius.only(
+                                                  topLeft: Radius.circular(15),
+                                                  topRight: Radius.circular(15),
+                                                ),
+                                              ),
+                                            ),
+                                          ),
+                                          Container(
+                                            width: _fractionSize,
+                                            height: _fractionSize,
+                                            margin: const EdgeInsets.only(bottom: 20),
+                                            child: Hero(tag: BebidaUtils.createBebidaTag(bebida), child: Image.network(bebida.imagem)),
+                                          )
+                                        ],
+                                      ),
+                                    ),
+                                    Container(
+                                      height: _BEBIDA_DESCRICAO_HEIGHT,
+                                      margin: const EdgeInsets.only(bottom: _BEBIDA_DESCRICAO_MARGIN_BOTTOM),
+                                      width: double.infinity,
+                                      decoration: BoxDecoration(
+                                        color: Colors.white,
+                                        boxShadow: kElevationToShadow[4],
+                                        borderRadius: const BorderRadius.only(
+                                          bottomLeft: Radius.circular(15),
+                                          bottomRight: Radius.circular(15),
+                                        ),
+                                      ),
+                                      child: Column(
+                                        crossAxisAlignment: CrossAxisAlignment.start,
+                                        mainAxisSize: MainAxisSize.min,
+                                        children: [
+                                          Expanded(
+                                            child: Padding(
+                                              padding: const EdgeInsets.only(left: 10, top: 5),
+                                              child: Row(
+                                                children: List.generate(
+                                                  QUANTIDADE_ESTRELAS_NOTA,
+                                                  (index) => Icon(
+                                                    Icons.star,
+                                                    color: bebida.nota > (VALOR_MAXIMO_PARA_NOTA / QUANTIDADE_ESTRELAS_NOTA) * index + 1
+                                                        ? Colors.yellow
+                                                        : Colors.grey.withOpacity(0.5),
+                                                  ),
+                                                ).toList(),
+                                              ),
+                                            ),
+                                          ),
+                                          Padding(
+                                            padding: const EdgeInsets.only(left: 10, right: 10, top: 20),
+                                            child: Text(
+                                              bebida.nome,
+                                              style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                                              overflow: TextOverflow.ellipsis,
+                                            ),
+                                          ),
+                                          Padding(
+                                            padding: const EdgeInsets.only(left: 10, right: 10, top: 5, bottom: 20),
+                                            child: Text(
+                                              "R\$ ${bebida.valor}",
+                                              style: const TextStyle(fontWeight: FontWeight.normal, fontSize: 16),
+                                              overflow: TextOverflow.ellipsis,
+                                            ),
+                                          ),
+                                        ],
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            );
+                          },
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
             ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20).copyWith(top: 40),
-              child: Align(
-                alignment: Alignment.topLeft,
-                child: Text(
-                  _categoriaAtualNome,
-                  style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 26),
+          ),
+          Positioned(
+            bottom: 0,
+            left: 0,
+            right: 0,
+            child: Container(
+              color: Colors.white,
+              child: const SafeArea(
+                child: Row(
+                  children: [
+                    _BottomBarItem(Icons.home),
+                    _BottomBarItem(Icons.wallet, notificationsCount: 2),
+                    _BottomBarItem(Icons.receipt),
+                    _BottomBarItem(Icons.person),
+                  ],
                 ),
               ),
             ),
-            Container(
-              margin: const EdgeInsets.only(top: 20),
-              child: Carrossel(
-                key: _carroseelKey,
-                currentIndex: _whiskysCurrentIndex,
-                onChanged: _changeWhiskysCurrentIndex,
-                childrens: categoriaMock.map((e) => CarrosselItem(e.nome)).toList(),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _BottomBarItem extends StatelessWidget {
+  const _BottomBarItem(this.icon, {this.notificationsCount});
+
+  final IconData icon;
+  final int? notificationsCount;
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Align(
+        child: SizedBox(
+          width: 40,
+          height: 40,
+          child: Stack(
+            children: [
+              Align(
+                child: Icon(
+                  icon,
+                  color: Colors.grey,
+                ),
               ),
-            ),
-            Container(
-              margin: const EdgeInsets.only(top: 30),
-              width: double.infinity,
-              height: _BEBIDA_CONTAINER_SIZE + _BEBIDA_DESCRICAO_HEIGHT + _BEBIDA_DESCRICAO_MARGIN_BOTTOM,
-              child: ListView.builder(
-                scrollDirection: Axis.horizontal,
-                itemCount: _categoriaAtualBebidas.length,
-                itemBuilder: (context, index) {
-                  final bebida = _categoriaAtualBebidas[index];
-                  return InkWell(
-                    onTap: () => _irParaDetalhes(bebida),
-                    child: Container(
-                      width: _fractionSize,
-                      margin: const EdgeInsets.symmetric(horizontal: 10),
-                      child: Column(
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          SizedBox(
-                            height: _BEBIDA_CONTAINER_SIZE,
-                            child: Stack(
-                              children: [
-                                Positioned(
-                                  bottom: 0,
-                                  child: Container(
-                                    height: _fractionSize,
-                                    width: _fractionSize,
-                                    decoration: BoxDecoration(
-                                      color: bebida.cor.withOpacity(0.4),
-                                      borderRadius: const BorderRadius.only(
-                                        topLeft: Radius.circular(15),
-                                        topRight: Radius.circular(15),
-                                      ),
-                                    ),
-                                  ),
-                                ),
-                                Container(
-                                  width: _fractionSize,
-                                  height: _fractionSize,
-                                  margin: const EdgeInsets.only(bottom: 20),
-                                  child: Hero(tag: BebidaUtils.createBebidaTag(bebida), child: Image.network(bebida.imagem)),
-                                )
-                              ],
-                            ),
-                          ),
-                          Container(
-                            height: _BEBIDA_DESCRICAO_HEIGHT,
-                            margin: const EdgeInsets.only(bottom: _BEBIDA_DESCRICAO_MARGIN_BOTTOM),
-                            width: double.infinity,
-                            decoration: BoxDecoration(
-                              color: Colors.white,
-                              boxShadow: kElevationToShadow[4],
-                              borderRadius: const BorderRadius.only(
-                                bottomLeft: Radius.circular(15),
-                                bottomRight: Radius.circular(15),
-                              ),
-                            ),
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              mainAxisSize: MainAxisSize.min,
-                              children: [
-                                Expanded(
-                                  child: Padding(
-                                    padding: const EdgeInsets.only(left: 10, top: 5),
-                                    child: Row(
-                                      children: List.generate(
-                                        QUANTIDADE_ESTRELAS_NOTA,
-                                        (index) => Icon(
-                                          Icons.star,
-                                          color: bebida.nota > (VALOR_MAXIMO_PARA_NOTA / QUANTIDADE_ESTRELAS_NOTA) * index + 1 ? Colors.yellow : Colors.grey.withOpacity(0.5),
-                                        ),
-                                      ).toList(),
-                                    ),
-                                  ),
-                                ),
-                                Padding(
-                                  padding: const EdgeInsets.only(left: 10, right: 10, top: 20),
-                                  child: Text(
-                                    bebida.nome,
-                                    style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
-                                    overflow: TextOverflow.ellipsis,
-                                  ),
-                                ),
-                                Padding(
-                                  padding: const EdgeInsets.only(left: 10, right: 10, top: 5, bottom: 20),
-                                  child: Text(
-                                    "R\$ ${bebida.valor}",
-                                    style: const TextStyle(fontWeight: FontWeight.normal, fontSize: 16),
-                                    overflow: TextOverflow.ellipsis,
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
-                        ],
+              if (notificationsCount != null)
+                Positioned(
+                  right: 1,
+                  top: 0,
+                  child: Container(
+                    width: 18,
+                    height: 18,
+                    decoration: const BoxDecoration(
+                      color: Colors.red,
+                      shape: BoxShape.circle,
+                    ),
+                    child: Center(
+                      child: Text(
+                        notificationsCount.toString(),
+                        style: const TextStyle(
+                          color: Colors.white,
+                        ),
                       ),
                     ),
-                  );
-                },
-              ),
-            ),
-          ],
+                  ),
+                ),
+            ],
+          ),
         ),
       ),
     );
